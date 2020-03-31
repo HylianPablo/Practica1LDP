cf5f01eb1d691e5bb372a666be746ca61a3b189f
javgato <javier.gaton@alumnos.uva.es>
2020-03-31 16:50:01 +0200

    borrar cosas de codigo sobrante y inicio pruebas

---
+++ pruebas/Prueba1.java
@@ -0,0 +1,7 @@
+// locc 1, locw 0, locp 6, locf 7
+public class Prueba1{
+	public static void main(String[] args){
+		System.out.println("Hola");
+		System.out.println("Que tal");
+	}
+}
---
+++ pruebas/Prueba2.java
@@ -0,0 +1,15 @@
+
+// locc 1, locw 8, locp 6, locf 15
+public class Prueba2{	
+	public static void main(String[] args){
+		System.out.println("Hola"); 	 
+		System.out.println("Que tal");	 	
+	}
+	
+ 
+	 
+ 	
+	  		 
+}
+
+
