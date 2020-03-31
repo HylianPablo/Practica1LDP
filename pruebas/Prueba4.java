39eb78f0051122f93799d55fbb20842e393359cd
Ivan <ivan.gonzalez.rincon@alumnos.uva.es>
2020-03-31 17:18:35 +0200

    subida prueba4

---
+++ pruebas/Prueba4.java
@@ -0,0 +1,30 @@
+/*LOCC = 0
+LOCP = 22
+LOCW = 4
+LOCF = 26
+Número ciclomático = 5*/
+public class Prueba4 {
+	public static void main(String args[]) {
+		if (3 > 4) {
+			System.out.println("Estoy en el if");
+		} else {
+			System.out.println("Estoy en el else");
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Imprimo el número " + i + " en el for");
+		}
+
+		int i = 0;
+		while (i < 5) {
+			System.out.println("Imprimo el número " + i + " en el while");
+			i++;
+		}
+
+		try {
+			System.out.println("Estoy en el try");
+		} catch (Exception e) {
+			System.out.println("Imprimo una excepcion");
+		}
+	}
+}
