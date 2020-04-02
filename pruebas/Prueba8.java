4c8eeb36a3a7d484af63335dea1962f85cb04c9b
Pablo Martínez <39129834+HylianPablo@users.noreply.github.com>
2020-04-02 10:30:39 +0200

    Update Prueba8.java

--- pruebas/Prueba8.java
+++ pruebas/Prueba8.java
@@ -3,6 +3,7 @@ public class Prueba8{
 	public static void main(String[] args){
 		System.out.println("Hola ? ? ? ? ? ? ? ? ? ");
 		System.out.println("Que tal");
+		System.out.println("a = 0 > 1 ? 0 : 1;");
 		int a;
 		a = 0 > 1 ? 0 : 1;
 		int b = (a > 3) || (5 < 4) ? 3 : 99;
